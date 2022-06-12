5e0cdcee695c5985b44d240aa7d21e2c8d3b808c
Parth-1011 <103803443+Parth-1011@users.noreply.github.com>
2022-06-12 21:37:16 +0530

    Create 21102031,assignment 4 output.py

---
+++ 21102031,assignment 4 output.py
@@ -0,0 +1,25 @@
+enter your marks:78
+you have scored  B grade.
+enter the year: 3600
+the year is a leap year
+Question 1: 9*5 = 45
+Right!
+Question 2: 1*8 = 8
+Right!
+Question 3: 3*8 = 24
+Right!
+Question 4: 5*7 = 33
+Wrong. The answer is  35
+Question 5: 6*7 = 42
+Right!
+Question 6: 5*6 = 30
+Right!
+Question 7: 4*4 = 16
+Right!
+Question 8: 8*8 = 66
+Wrong. The answer is  64
+Question 9: 2*7 = 14
+Right!
+Question 10: 2*5 = 10
+Right!
+Number of candies in the box are 177
